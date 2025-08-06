95d31b82caaf4bedb498aed8c9512a37c168d30d
Natan Mauricio Santos <natanmauriciosantos@hotmail.com>
2025-08-05 21:20:10 -0300

    Create Manipulador_de_Listas.py
    Programa que manipula uma lista de números, inserida pelo usuário, em Python

---
+++ Manipulador_de_Listas.py
@@ -0,0 +1,10 @@
+--------------
+CLASSE
+--------------
+"""
+#Cria a classe lista
+class Manipulador_de_Lista:
+    
+    #Cria o método contrutor da classe
+    def __init__(self):
+        self.lista = []
