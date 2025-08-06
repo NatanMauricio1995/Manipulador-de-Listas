331d4d4be0af99451532f677f7a43db0b627a3c1
Natan Mauricio Santos <natanmauriciosantos@hotmail.com>
2025-08-05 21:21:01 -0300

    Update Manipulador_de_Listas.py
    #Adiciona um elemento no final da lista

--- Manipulador_de_Listas.py
+++ Manipulador_de_Listas.py
@@ -8,3 +8,7 @@ class Manipulador_de_Lista:
     #Cria o método contrutor da classe
     def __init__(self):
         self.lista = []
+
+    #Adiciona um elemento no final da lista
+    def adicionar_elemento(self,elemento):
+        self.lista.append(elemento)
